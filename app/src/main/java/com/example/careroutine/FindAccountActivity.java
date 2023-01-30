447c5f71bf8c33b807654827074c46f9baf0bbd9
장혜진 <g2nie>
2023-01-30 14:58:32 +0900

    23/01/30 FindAccount g2nie

---
+++ app/src/main/java/com/example/careroutine/FindAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.careroutine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_account);
+    }
+}

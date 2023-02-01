e47f31166706e7ed9c7a2caea3ab155a910a149a
ICanSeeTheHalo <201844087@itc.ac.kr>
2023-02-02 00:42:10 +0900

    23/02/02 MyPage ICanSeeTheHalo

---
+++ app/src/main/java/com/example/careroutine/MemberCertify.java
@@ -0,0 +1,14 @@
+package com.example.careroutine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberCertify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_certify);
+    }
+}

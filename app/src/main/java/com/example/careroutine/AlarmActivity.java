09ce0ec6f7c4dbacb734f4afbf72bfd3c5649e9f
ICanSeeTheHalo <201844087@itc.ac.kr>
2023-02-01 22:30:01 +0900

    23/02/01 PharmacyMap ICanSeeTheHalo

--- app/src/main/java/com/example/careroutine/AlarmActivity.java
+++ app/src/main/java/com/example/careroutine/AlarmActivity.java
@@ -2,7 +2,14 @@ package com.example.careroutine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
 
 public class AlarmActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
     }
 }

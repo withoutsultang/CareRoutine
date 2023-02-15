737c4d6c10bcdaba044bc5c866cee5de4d5b42c6
장혜진 <g2nie>
2023-02-15 11:39:02 +0900

    edit login layout

--- app/src/main/java/com/example/careroutine/view/LoginActivity.java
+++ app/src/main/java/com/example/careroutine/view/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.example.careroutine.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.example.careroutine.R;
+import com.example.careroutine.databinding.ActivityLoginBinding;
+import com.example.careroutine.viewmodel.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.setLifecycleOwner(this);
+
+        loginViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(LoginViewModel.class);
+        binding.setViewModel(loginViewModel);
+
     }
 }

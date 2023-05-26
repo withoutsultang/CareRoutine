0d03b203b1f42efb77e95b9b8e4aa0d5bdae248e
ICanSeeTheHalo <201844087@itc.ac.kr>
2023-05-26 10:42:19 +0900

    firebase 설정

---
+++ app/src/main/java/com/withoutsultang/careroutine/view/model/MemberDAO.java
@@ -0,0 +1,10 @@
+package com.withoutsultang.careroutine.view.model;
+
+import android.widget.EditText;
+
+public class MemberDAO {
+
+    private static String IP_ADDRESS = "13.124.123.123";
+    private static String TAG = "phptest";
+
+}
---
+++ app/src/main/java/com/withoutsultang/careroutine/view/model/SignUpRepository.java
@@ -0,0 +1,5 @@
+package com.withoutsultang.careroutine.view.model;
+
+public class SignUpRepository {
+    public boolean agree;
+}

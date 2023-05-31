575fea54d979b13242165aea9a5480c1be110707
inhatc <201844087@itc.ac.kr>
2023-05-31 15:02:08 +0900

    temp

--- app/src/main/java/com/withoutsultang/careroutine/viewmodel/FindAccountViewModel.java
+++ app/src/main/java/com/withoutsultang/careroutine/viewmodel/FindAccountViewModel.java
@@ -6,36 +6,43 @@ import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class FindAccountViewModel extends ViewModel {
 
-    //파이어베이스 데이터베이스 사용 선언
     FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    //데이터베이스 path만 똑같이
     DatabaseReference userRef = database.getReference("users");
 
     //xml에서 데이터바인딩한 변수 관찰가능한 변수로 생성
-    public ObservableField<String> fidemail = new ObservableField<>("");
-    public ObservableField<String> fidbirth = new ObservableField<>("");
+    public ObservableField<String> femlname = new ObservableField<>("");
+    public ObservableField<String> femlbirth = new ObservableField<>("");
+    public ObservableField<String> fpwemail = new ObservableField<>("");
+    public ObservableField<String> fpwcode = new ObservableField<>("");
+    public ObservableField<String> nowpw = new ObservableField<>("");
+    public ObservableField<String> changepw = new ObservableField<>("");
 
-    //화면전환시 필요
     private Context context;
 
-    //화면전환시 필요
     public FindAccountViewModel(Context context) {
         this.context = context;
     }
 
-    public void onClickSearch() {
-        /*String text1 = fidemail.get();
-        String text2 = fidbirth.get();*/
-
-        //파이어베이스 리얼타임데이터베이스
-        /*userRef.child(text1).setValue(text2);*/
-
-        /*if (text1 != null && text2 != null) {
-            databaseReference.child(text1).setValue(text2);
-        }*/
+    public void onClickSearch(String name, String birthday) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        Query query = databaseReference.orderByChild("name").equalTo(name);
     }
+
+    public void onClickSend() {
+
+    }
+
+    public void onClickCheckCode() {
+
+    }
+
+    public void onClickCheckpw() {
+
+    }
+
 }

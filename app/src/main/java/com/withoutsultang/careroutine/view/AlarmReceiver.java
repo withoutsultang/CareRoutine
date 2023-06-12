55d5e9f196ad0d7df30dd1f3e84a994c9cb97614
G2nie <202144081@itc.ac.kr>
2023-06-12 14:43:30 +0900

    create alarm popup

--- app/src/main/java/com/withoutsultang/careroutine/view/AlarmReceiver.java
+++ app/src/main/java/com/withoutsultang/careroutine/view/AlarmReceiver.java
@@ -8,7 +8,11 @@ import android.widget.Toast;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        // 알람이 울렸을 때 실행될 동작을 여기에 정의합니다.
         Toast.makeText(context, "알람이 울렸습니다!", Toast.LENGTH_SHORT).show();
+
+        // 팝업 출력
+        Intent popupIntent = new Intent(context, AlarmPopupActivity.class);
+        popupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(popupIntent);
     }
 }

1bc20071687aef6a453880d35371fd3a441e36b3
김건우 <201944085p@gmail.com>
2023-06-13 03:01:10 +0900

    recaptcha 완료

--- app/src/main/java/com/withoutsultang/careroutine/view/LoginActivity.java
+++ app/src/main/java/com/withoutsultang/careroutine/view/LoginActivity.java
@@ -1,28 +1,80 @@
 package com.withoutsultang.careroutine.view;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.careroutine.R;
 import com.example.careroutine.databinding.ActivityLoginBinding;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
 import com.withoutsultang.careroutine.viewmodel.LoginViewModel;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks {
 
     private LoginViewModel viewModel;
+    Button button;
+    GoogleApiClient mGoogleApiClient;
+    String SiteKey = "6Lc4wWkmAAAAAClfBmHw9Qsn4ulSJhPbiid_x8Pc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityLoginBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
         viewModel = new LoginViewModel(this);
 
         binding.setViewModel(viewModel);
+
+
+        button = findViewById(R.id.btnLogin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SafetyNet.SafetyNetApi.verifyWithRecaptcha(mGoogleApiClient, SiteKey)
+                        .setResultCallback(new ResultCallback<SafetyNetApi.RecaptchaTokenResult>() {
+                            @Override
+                            public void onResult(@NonNull SafetyNetApi.RecaptchaTokenResult recaptchaTokenResult) {
+                                Status status = recaptchaTokenResult.getStatus();
+                                if ((status != null) && status.isSuccess()) {
+                                    // Indicates communication with reCAPTCHA service was
+                                    // successful. Use result.getTokenResult() to get the
+                                    // user response token if the user has completed
+                                    // the CAPTCHA.
+                                    viewModel.onClickLogin();
+                                } else {
+                                    // An error occurred while communicating with the
+                                    // reCAPTCHA service. Refer to the status code to
+                                    // handle the error appropriately.
+
+                                    Toast.makeText(LoginActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(SafetyNet.API)
+                .addConnectionCallbacks(LoginActivity.this)
+                .build();
+        mGoogleApiClient.connect();
+
 
         viewModel.getNavigateToSignUpActivity().observe(this, navigate -> {
             if (navigate) {
@@ -45,4 +97,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
 }
--- app/src/main/java/com/withoutsultang/careroutine/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/withoutsultang/careroutine/viewmodel/LoginViewModel.java
@@ -1,19 +1,43 @@
 package com.withoutsultang.careroutine.viewmodel;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+class ViewModelExecutor implements Executor {
+    @Override
+    public void execute(Runnable command) {
+        command.run();
+    }
+}
+
+
 public class LoginViewModel extends ViewModel {
+
+    private ViewModelExecutor executor;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference userRef = database.getReference("users");
@@ -30,6 +54,7 @@ public class LoginViewModel extends ViewModel {
     private static MutableLiveData<Boolean> navigateToMainActivity = new MutableLiveData<>();
 
     public LoginViewModel(Context context) {
+        executor = new ViewModelExecutor();
         this.context = context;
     }
 
@@ -46,6 +71,43 @@ public class LoginViewModel extends ViewModel {
 
     }
 
+
+
+//    public void recaptcha(Activity activity) {
+//        SafetyNet.getClient(activity).verifyWithRecaptcha("6Lc4wWkmAAAAAClfBmHw9Qsn4ulSJhPbiid_x8Pc")
+//                .addOnSuccessListener(executor,
+//                        new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+//                            @Override
+//                            public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+//                                // Indicates communication with reCAPTCHA service was
+//                                // successful.
+//                                String userResponseToken = response.getTokenResult();
+//                                if (!userResponseToken.isEmpty()) {
+//                                    onClickLogin();
+//                                    // Validate the user response token using the
+//                                    // reCAPTCHA siteverify API.
+//                                }
+//                            }
+//                        })
+//                .addOnFailureListener(executor, new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        if (e instanceof ApiException) {
+//                            // An error occurred when communicating with the
+//                            // reCAPTCHA service. Refer to the status code to
+//                            // handle the error appropriately.
+//                            ApiException apiException = (ApiException) e;
+//                            int statusCode = apiException.getStatusCode();
+//                            Log.d(TAG, "Error: " + CommonStatusCodes
+//                                    .getStatusCodeString(statusCode));
+//                        } else {
+//                            // A different, unknown type of error occurred.
+//                            Log.d(TAG, "Error: " + e.getMessage());
+//                        }
+//                    }
+//                });
+//    }
+
     public void onClickLogin() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         String inputemail = email.get();
@@ -57,7 +119,7 @@ public class LoginViewModel extends ViewModel {
                     navigateToMainActivity();
                 }
                 else {
-                    Toast.makeText(context, "로그인 샐패", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "로그인 실패", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -87,3 +149,4 @@ public class LoginViewModel extends ViewModel {
         navigateToMainActivity.setValue(true);
     }
 }
+

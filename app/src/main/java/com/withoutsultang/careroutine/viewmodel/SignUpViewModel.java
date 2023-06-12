575c3b574c0d2ea8c7ee49a43765bdbab570d241
ICanSeeTheHalo <201844087@itc.ac.kr>
2023-06-12 15:39:48 +0900

    Temp

--- app/src/main/java/com/withoutsultang/careroutine/viewmodel/SignUpViewModel.java
+++ app/src/main/java/com/withoutsultang/careroutine/viewmodel/SignUpViewModel.java
@@ -1,6 +1,12 @@
 package com.withoutsultang.careroutine.viewmodel;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.databinding.ObservableField;
@@ -8,6 +14,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.careroutine.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -34,6 +41,54 @@ public class SignUpViewModel extends ViewModel {
     }
 
     public void onClickSignUp() {
+        String password = ePW.get();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(context, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 비밀번호 길이 확인
+        if (password.length() < 8 || password.length() > 12) {
+            Toast.makeText(context, "8 ~ 12자리의 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 비밀번호에 한글, 공백 확인
+        if (password.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣\\s]+.*")) {
+            Toast.makeText(context, "비밀번호에는 한글과 공백을 입력할 수 없습니다", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 비밀번호에 영어, 숫자, 특수기호 확인
+        if (!password.matches(".*[a-zA-Z0-9~!@#$%^&*()_+`\\-={}|\\[\\]:\";'<>?,./\\\\]+.*")) {
+            Toast.makeText(context, "비밀번호는 영어 알파벳(대소문자 구별), 숫자, 특수기호만 입력 가능합니다", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 이름 입력 확인
+        String name = eName.get();
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(context, "이름을 입력하세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 이름 길이 확인
+        if (name.length() < 2) {
+            Toast.makeText(context, "2글자 이상 입력하세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 이름에 한글 이외의 글자 확인
+        if (!name.matches("^[가-힣]*$")) {
+            Toast.makeText(context, "한글만 입력하세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 생년월일 입력 확인
+        String birthday = eBirth.get();
+        if (TextUtils.isEmpty(birthday)) {
+            Toast.makeText(context, "생년월일을 입력하세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 생년월일 형식 확인
+        if (!birthday.matches("\\d{4}/\\d{2}/\\d{2}")) {
+            Toast.makeText(context, "올바른 생년월일 형식이 아닙니다", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -53,6 +108,23 @@ public class SignUpViewModel extends ViewModel {
                         Toast.makeText(context, "회원가입 실패", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+    public void showTermsPopup(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.terms_dialog, null);
+
+        TextView tvTermsContent = view.findViewById(R.id.tvTermsContent);
+        // 약관 내용을 설정하세요
+        tvTermsContent.setText("약관 내용을 입력하세요");
+
+        Button btnTermsAgree = view.findViewById(R.id.btnTermsAgree);
+        btnTermsAgree.setOnClickListener(v -> {
+        });
+
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     public static LiveData<Boolean> getNavigateToLoginActivity() {
